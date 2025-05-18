349f792a3ac2207e0a913e07996f252319078e2f
zhou-liurong <19551230598@163.com>
2025-05-18 16:12:29 +0800

    哈夫曼编码

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -5,6 +5,73 @@
 using namespace std;
 
 //#include <iostream>
+//#include<queue>
+//using namespace std;
+//
+//int main()
+//{
+//    priority_queue<long long, vector<long long>, greater<long long>> pq;
+//    int n = 0;
+//    cin >> n;
+//    for (int i = 0; i < n; i++)
+//    {
+//        long long num;
+//        cin >> num;
+//        pq.push(num);
+//    }
+//
+//    long long ret = 0;
+//    while (pq.size() > 1)
+//    {
+//        long long a = pq.top();
+//        pq.pop();
+//        long long b = pq.top();
+//        pq.pop();
+//
+//        ret += a + b;
+//        pq.push(a + b);
+//    }
+//    cout << ret << endl;
+//    return 0;
+//}
+//// 64 位输出请用 printf("%lld")
+
+//#include <iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//    string s, t;
+//    cin >> s >> t;
+//
+//    int n = s.size(), m = t.size();
+//    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+//
+//    for (int i = 0; i <= n; i++) dp[i][0] = i;
+//    for (int i = 0; i <= m; i++) dp[0][i] = i;
+//
+//    for (int i = 1; i <= n; i++)
+//    {
+//        for (int j = 1; j <= m; j++)
+//        {
+//            if (s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+//
+//            else
+//            {
+//                dp[i][j] = min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+//            }
+//        }
+//    }
+//
+//    cout << dp[n][m] << endl;
+//
+//    return 0;
+//}
+//// 64 位输出请用 printf("%lld")
+
+//#include <iostream>
 //#include<vector>
 //using namespace std;
 //
