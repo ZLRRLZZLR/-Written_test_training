1be715fe3d72e977d3dbaf8a049766ef1e4f548e
zhou-liurong <19551230598@163.com>
2025-06-30 18:23:29 +0800

    二叉树的中序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -11,6 +11,35 @@
 // */
 //class Solution {
 //public:
+//    bool _isSymmetric(TreeNode* q, TreeNode* p)
+//    {
+//        if (q == nullptr && p == nullptr)
+//            return true;
+//        if (q == nullptr || p == nullptr)
+//            return false;
+//        if (q->val != p->val)
+//            return false;
+//
+//        return _isSymmetric(q->left, p->right) && _isSymmetric(q->right, p->left);
+//    }
+//    bool isSymmetric(TreeNode* root) {
+//        if (root == nullptr) return true;
+//        return _isSymmetric(root->left, root->right);
+//    }
+//};
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
 //    int maxDepth(TreeNode* root) {
 //        if (root == nullptr) return 0;
 //        int leftheight = maxDepth(root->left);
