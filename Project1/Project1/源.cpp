74a8ff4866595cb3ae710ddd79b65f6734cfdf2a
zhou-liurong <19551230598@163.com>
2025-07-05 23:40:26 +0800

    爬楼梯

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -6,6 +6,23 @@
 #include<cassert>
 
 using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        for (int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
 
 //void InsertSort(int* a,int n)
 //{
