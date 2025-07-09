207569d14681bcac3f745ca7470848e03b90e2ef
zhou-liurong <19551230598@163.com>
2025-07-09 22:45:24 +0800

    杨辉三角

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -7,7 +7,26 @@
 
 using namespace std;
 
-
+//
+//class Solution {
+//public:
+//    vector<vector<int>> generate(int numRows) {
+//        vector<vector<int>> vv(numRows);
+//        for (int i = 0; i < numRows; i++)
+//        {
+//            vv[i].resize(i + 1, 1);
+//        }
+//        for (int i = 2; i < numRows; i++)
+//        {
+//            for (int j = 1; j < i; j++)
+//            {
+//                vv[i][j] = vv[i - 1][j - 1] + vv[i - 1][j];
+//            }
+//        }
+//
+//        return vv;
+//    }
+//};
 ///**
 // * Definition for singly-linked list.
 // * struct ListNode {
