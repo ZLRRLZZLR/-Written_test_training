e6ea0ed3cd223c6d42b7f57326e04ae6a7692992
zhou-liurong <19551230598@163.com>
2025-07-12 21:11:38 +0800

    翻转二叉树

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -12,6 +12,32 @@
 // */
 //class Solution {
 //public:
+//    void _invertTree(TreeNode* root) {
+//        if (root == nullptr) return;
+//        TreeNode* cur = root->left;
+//        root->left = root->right;
+//        root->right = cur;
+//        invertTree(root->left);
+//        invertTree(root->right);
+//    }
+//    TreeNode* invertTree(TreeNode* root) {
+//        _invertTree(root);
+//        return root;
+//    }
+//};
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
 //    vector<int> ret;
 //    void _inorderTraversal(TreeNode* root) {
 //        if (root == nullptr) return;
