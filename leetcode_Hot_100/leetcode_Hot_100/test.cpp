9682d20b4e666d6e7fb6fe8b4bfd8d504b41f543
zhou-liurong <19551230598@163.com>
2025-07-13 21:59:41 +0800

    二叉树的层序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -10,6 +10,45 @@
 // *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 // * };
 // */
+//
+//class Solution {
+//public:
+//    vector<vector<int>> levelOrder(TreeNode* root) {
+//        vector<vector<int>> vv;
+//        queue<TreeNode*> q;
+//        if (root == nullptr) return vv;
+//        q.push(root);
+//        int levelsize = 1;
+//        while (q.size()) {
+//            vector<int> v;
+//            while (levelsize--) {
+//                TreeNode* cur = q.front();
+//                q.pop();
+//                v.push_back(cur->val);
+//                if (cur->left) {
+//                    q.push(cur->left);
+//                }
+//                if (cur->right) {
+//                    q.push(cur->right);
+//                }
+//            }
+//            vv.push_back(v);
+//            levelsize = q.size();
+//        }
+//        return vv;
+//    }
+//};
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
 //class Solution {
 //public:
 //    void _invertTree(TreeNode* root) {
