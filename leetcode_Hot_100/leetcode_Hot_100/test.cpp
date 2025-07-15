4d22252e5c47c619f8eb999d32e92221df82a2cc
zhou-liurong <19551230598@163.com>
2025-07-15 20:25:38 +0800

    回文链表

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -1,4 +1,31 @@
 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        ListNode* cur = head;
+        while (cur) {
+            v.push_back(cur->val);
+            cur = cur->next;
+        }
+        for (int left = 0, right = v.size() - 1; left <= right; left++, right--) {
+            if (v[left] != v[right]) return false;
+        }
+        return true;
+    }
+};
+
 ///**
 // * Definition for a binary tree node.
 // * struct TreeNode {
