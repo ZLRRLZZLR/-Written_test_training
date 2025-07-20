ee674efe2c52c3bc81057eb92a83f95639fc2e73
zhou-liurong <19551230598@163.com>
2025-07-20 22:51:18 +0800

    环形链表II

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -1,3 +1,32 @@
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     ListNode *next;
+// *     ListNode(int x) : val(x), next(NULL) {}
+// * };
+// */
+//class Solution {
+//public:
+//    ListNode* detectCycle(ListNode* head) {
+//        ListNode* fast = head;
+//        ListNode* slow = head;
+//        while (fast && fast->next) {
+//            slow = slow->next;
+//            fast = fast->next->next;
+//            if (slow == fast) {
+//                ListNode* meet = slow;
+//                while (head != meet) {
+//                    head = head->next;
+//                    meet = meet->next;
+//                }
+//                return meet;
+//            }
+//
+//        }
+//        return nullptr;
+//    }
+//};
 //class Solution {
 //public:
 //    int longestConsecutive(vector<int>& nums) {
