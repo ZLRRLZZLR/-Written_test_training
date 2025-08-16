e5d776f85e0a99a32bdbcf75bdb5623eba1b1b51
zhou-liurong <19551230598@163.com>
2025-08-16 22:15:29 +0800

    寻找峰值、找数字大小

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -1,3 +1,49 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int left = 0,right = nums.size() - 1;
+//         if(nums.size() == 1) return left;
+
+//         while(left < right){
+//             int mid = left + (right - left + 1) / 2;
+//             if(nums[mid] > nums[mid - 1]){
+//                 left = mid;
+//             }
+//             else{
+//                 right = mid - 1;
+//             }
+//         }
+//         return right;
+//     }
+// };
+
+
+// /** 
+//  * Forward declaration of guess API.
+//  * @param  num   your guess
+//  * @return 	     -1 if num is higher than the picked number
+//  *			      1 if num is lower than the picked number
+//  *               otherwise return 0
+//  * int guess(int num);
+//  */
+
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//         int left = 1,right = n;
+//         while(left < right){
+//             int mid = left + (right - left + 1) / 2;
+//             if(guess(mid) == -1){
+//                 right = mid - 1;
+//             }
+//             else{
+//                 left = mid;
+//             }
+
+//         }
+//         return left;
+//     }
+// };
 
 //class Solution {
 //public:
