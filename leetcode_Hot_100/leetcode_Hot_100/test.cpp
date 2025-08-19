34d5143bc940ff6a8aa3953aa1656938f7bf9c2e
zhou-liurong <19551230598@163.com>
2025-08-19 20:39:18 +0800

    判断子序列

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int s1 = 0,t1 = 0;
+//         while(s1 < s.size() && t1 < t.size()){
+//             if(s[s1] == t[t1]){
+//                 s1++;
+//                 t1++;
+//             }
+//             else{
+//                 t1++;
+//             }
+//         }
+//         if(s1 < s.size()) return false;
+//         return true;
+//     }
+// };
+
+
 //class Solution {
 //public:
 //    string removeStars(string s) {
