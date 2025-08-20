a143afdffb6faa5397b55017defb3afdd114fe59
zhou-liurong <19551230598@163.com>
2025-08-20 21:39:22 +0800

    二叉树的最大深度

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,28 @@
 using namespace std;
 
 
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(root == nullptr) return 0;
+//         int leftheight = maxDepth(root->left);
+//         int rightheight = maxDepth(root->right);
+//         return leftheight > rightheight ? leftheight + 1: rightheight + 1;
+//     }
+// };
+
 // class Solution {
 // public:
 //     bool isSubsequence(string s, string t) {
