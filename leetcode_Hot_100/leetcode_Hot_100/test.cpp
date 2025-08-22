0455e8e8b2eeeb6721476eb84823c5f91cc32031
zhou-liurong <19551230598@163.com>
2025-08-22 21:14:14 +0800

    反转链表

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -5,6 +5,32 @@
 
 using namespace std;
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* newhead = new ListNode;
+//         ListNode* cur = head;
+//         while(cur){
+//             ListNode* tmp = cur;
+//             cur = cur->next;
+//             tmp->next = newhead->next;
+//             newhead->next = tmp;
+//         }
+//         return newhead->next;
+//     }
+// };
+
+
 // class Solution {
 // public:
 //     int minCostClimbingStairs(vector<int>& cost) {
