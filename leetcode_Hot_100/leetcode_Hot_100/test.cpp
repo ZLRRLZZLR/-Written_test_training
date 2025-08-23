629c3c29574f3a95d162c5b4d607c0111f964c7d
zhou-liurong <19551230598@163.com>
2025-08-23 23:35:05 +0800

    交替字符串

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -5,6 +5,29 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+//         int l1 = 0,l2 = 0;
+//         string ret;
+//         while(l1 < word1.size() && l2 < word2.size()){
+//             ret += word1[l1++];
+//             ret += word2[l2++];
+//         }
+
+//         while(l1 < word1.size()){
+//             ret += word1[l1++];
+//         }
+
+//         while(l2 < word2.size()){
+//             ret += word2[l2++];
+//         }
+//         return ret;
+//     }
+// };
+
+
+
 // /**
 //  * Definition for singly-linked list.
 //  * struct ListNode {
