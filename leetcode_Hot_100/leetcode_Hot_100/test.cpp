5fc7c308d65ce6cc6b1b38c3d0a9b978a63317ba
zhou-liurong <19551230598@163.com>
2025-08-28 22:50:45 +0800

    删除链表的倒数第N个节点

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -5,6 +5,53 @@
 
 using namespace std;
 
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         if(head->next == nullptr) {
+//             delete head;
+//             return nullptr;
+//         }
+
+//         ListNode* prev = nullptr,* slow = head,* fast = head;
+
+//         int num = n;
+//         while(num--){
+//             fast = fast->next;
+//         }
+
+//         while(fast){
+//             fast = fast->next;
+//             prev = slow;
+//             slow = slow->next; 
+//         }
+
+//         if(prev == nullptr){
+//             prev = slow;
+//             slow = slow->next;
+//             delete prev;
+//             return slow;
+//         }
+
+
+//         prev->next = slow->next;
+
+//         delete slow;
+//         return head;
+//     }
+// };
+
 // /**
 //  * Definition for singly-linked list.
 //  * struct ListNode {
