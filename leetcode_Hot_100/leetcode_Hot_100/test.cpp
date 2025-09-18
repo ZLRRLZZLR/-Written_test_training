66a12a9fc1ed4926dd1f76ad95660fd45a140d54
zhou-liurong <19551230598@163.com>
2025-09-18 20:43:44 +0800

    随机链表的复制-哈希表法

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -26,6 +26,61 @@ using namespace std;
 // class Solution {
 // public:
 //     Node* copyRandomList(Node* head) {
+
+//         if(head == nullptr) return nullptr;
+
+//         unordered_map<Node*,Node*>hash;//原节点与复制节点一一对应
+//         Node* cur = head;
+
+//         while(cur)
+//         {
+//             Node* node = new Node(cur->val);
+//             hash[cur] = node;
+//             cur = cur->next;
+//         }
+
+//         cur = head;
+//         Node* newhead = hash[cur];
+
+//         while(cur)
+//         {
+//             hash[cur]->next = hash[cur->next];
+
+//             if(cur->random)
+//             {
+//                 hash[cur]->random = hash[cur->random];
+//             }
+//             else
+//             {
+//                 hash[cur]->random = nullptr;
+//             }
+//             cur = cur->next;
+//         }
+
+
+//         return newhead;
+//     }
+// };
+
+// /*
+// // Definition for a Node.
+// class Node {
+// public:
+//     int val;
+//     Node* next;
+//     Node* random;
+    
+//     Node(int _val) {
+//         val = _val;
+//         next = NULL;
+//         random = NULL;
+//     }
+// };
+// */
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
 //         if(head == nullptr) return head;
 //         Node* cur = head;
 
