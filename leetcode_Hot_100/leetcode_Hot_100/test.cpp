fea9952de2f9090f26529443f1d7cdb3f007c72f
zhou-liurong <19551230598@163.com>
2025-09-24 13:55:56 +0800

    子集

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,25 @@ using namespace std;
 
 // class Solution {
 // public:
+//     vector<int> path;
+//     vector<vector<int>> ret;
+//     vector<vector<int>> subsets(vector<int>& nums) {
+
+//         dfs(nums,0);
+//         return ret;
+//     }
+//     void dfs(vector<int>& nums,int pos){
+//         ret.push_back(path);
+//         for(int i = pos;i < nums.size();i++){
+//             path.push_back(nums[i]);
+//             dfs(nums,i + 1);
+//             path.pop_back();
+//         }
+//     }
+// };
+
+// class Solution {
+// public:
 //     void moveZeroes(vector<int>& nums) {
 //         int n = nums.size();
 //         for(int left = -1,right = 0;right < n;right++)
