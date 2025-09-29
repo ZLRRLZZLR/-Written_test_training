e727d82eee119adbace82fc51ac60221d04ce5ba
zhou-liurong <19551230598@163.com>
2025-09-29 20:24:43 +0800

    打家劫舍

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,55 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 0) return 0;
+//         vector<int> dp(n + 1,0);
+//         dp[1] = nums[0];
+//         for(int i = 2;i <= n;i++)
+//         {
+//             dp[i] = max(dp[i - 1],dp[i - 2] + nums[i - 1]);
+//         }
+
+//         return dp[n];
+//     }
+// };
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+//         return dfs(nums,0,nums.size() - 1);
+//     }
+//     //因为构建平衡二叉树，这里为了方便直接选取中间点做根节点
+//     TreeNode* dfs(vector<int>& nums,int left,int right)
+//     {
+//         if(left > right)
+//         {
+//             return nullptr;
+//         }
+
+//         int mid = left + (right - left) / 2;
+
+//         TreeNode* root = new TreeNode(nums[mid]);
+//         root->left = dfs(nums,left,mid - 1);
+//         root->right = dfs(nums,mid + 1,right);
+//         return root;
+//     }
+// };
+
 
 // /**
 //  * Definition for a binary tree node.
