46e0da984acf295af1bd888b6d8d9e4c4b3b3c49
zhou-liurong <19551230598@163.com>
2025-10-03 22:39:33 +0800

    二叉树展开为链表

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -18,6 +18,34 @@ using namespace std;
 //  * };
 //  */
 // class Solution {
+//     TreeNode* head;
+// public:
+//     void flatten(TreeNode* root) {
+//         if(root == nullptr)
+//         {
+//             return ;
+//         }
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->left = nullptr;
+//         root->right = head;
+//         head = root;
+//     }
+// };
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
 //     vector<int> ret;
 // public:
 //     void dfs(TreeNode* root,int depth)
