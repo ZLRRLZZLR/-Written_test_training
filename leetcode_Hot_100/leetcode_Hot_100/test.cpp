8ecca9243814aec4098fe8605f6297c62d7b2e08
zhou-liurong <19551230598@163.com>
2025-10-06 23:34:33 +0800

    二叉树的中序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -18,6 +18,32 @@ using namespace std;
 //  * };
 //  */
 // class Solution {
+// public:
+// vector<int> ret;
+//     void _inorderTraversal(TreeNode* root){
+//         if(root == nullptr) return ;
+//         _inorderTraversal(root->left);
+//         ret.push_back(root->val);
+//         _inorderTraversal(root->right);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root){
+//         _inorderTraversal(root);
+//         return ret;
+//     }
+// };
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
 //     TreeNode* _buildTree(vector<int>& preorder, vector<int>& inorder,int& previ,int begini,int endi)
 //     {
 //         if(begini > endi)
