1ab360bf53aae0e73f1d6ddda0e3a22ccc4fb673
zhou-liurong <19551230598@163.com>
2025-10-17 14:39:31 +0800

    下一个数列

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,28 @@ using namespace std;
 
 // class Solution {
 // public:
+//     void nextPermutation(vector<int>& nums) {
+//         int n = nums.size();
+//         int i = n - 2;
+//         while(i >= 0 && nums[i] >= nums[i + 1]){
+//             --i;
+//         }
+
+//         if(i >= 0){
+//             int j = n - 1;
+//             while(nums[j] <= nums[i]){
+//                 j--;
+//             }
+//             swap(nums[i],nums[j]);
+//         }
+
+//         reverse(nums.begin() + i + 1,nums.end());
+//     }
+// };
+
+
+// class Solution {
+// public:
 //     int findDuplicate(vector<int>& nums) {
 //         //基环树，转化为环形链表、
 //         int slow = 0,fast = 0;
