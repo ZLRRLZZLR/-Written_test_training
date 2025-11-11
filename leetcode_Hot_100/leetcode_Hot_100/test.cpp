ab09f4b4b865765eed37ff45004859e69192fb22
zhou-liurong <19551230598@163.com>
2025-11-11 21:01:46 +0800

    中心拓展法

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -7,6 +7,42 @@
 using namespace std;
 
 
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         int n = s.size();
+//         int ans_left = 0,ans_right = 0;
+
+//         //奇回文串
+//         for(int i = 0;i < n;i++){
+//             int l = i,r = i;
+//             while(l >= 0 && r < n && s[l] == s[r]){
+//                 l--;
+//                 r++;
+//             }
+//             if(r - l - 1 > ans_right - ans_left){
+//                 ans_left = l + 1;
+//                 ans_right = r;
+//             }
+//         }
+
+//         //偶回文串
+//         for(int i = 0;i < n;i++){
+//             int l = i,r = i + 1;
+//             while(l >= 0 && r < n && s[l] == s[r]){
+//                 l--;
+//                 r++;
+//             }
+//             if(r - l - 1 > ans_right - ans_left){
+//                 ans_left = l + 1;
+//                 ans_right = r;
+//             }
+//         }
+
+//         return s.substr(ans_left,ans_right - ans_left);
+//     }
+// };
+
 
 // class Solution {
 //     int tar = 0;
