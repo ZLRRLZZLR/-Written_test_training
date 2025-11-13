51669ec80d110b20b5ac249a7656d46fe8733b87
zhou-liurong <19551230598@163.com>
2025-11-13 21:21:14 +0800

    [刷题日志]:三数之和

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,42 @@
 
 using namespace std;
 
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<vector<int>> ret;
+//         int n = nums.size();
+
+//         for(int i = 0;i < n - 2;i++){
+//             int x = nums[i];
+//             if(i && x == nums[i - 1]) continue;
+//             if(x + nums[i + 1] + nums[i + 2] > 0) break;
+//             if(x + nums[n - 2] + nums[n - 1] < 0) continue;
+
+//             int j = i + 1,k = n - 1;
+//             while(j < k){
+//                 int s = x + nums[j] + nums[k];
+//                 if(s > 0){
+//                     k--;
+//                 }
+//                 else if(s < 0){
+//                     j++;
+//                 }
+//                 else{
+//                     ret.push_back({x,nums[j],nums[k]});
+//                     for(j++;j < k && nums[j] == nums[j - 1];j++);
+//                     for(k--;j < k && nums[k] == nums[k + 1];k--);
+//                 }
+//             }
+//         }
+
+//         return ret;
+//     }
+// };
+
+
 // class Solution {
 //     vector<string> ret;
 //     int _n;
