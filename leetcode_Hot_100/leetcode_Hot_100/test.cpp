84403e944ee0aa7a3d04b32b59250d08ce7b9b85
zhou-liurong <19551230598@163.com>
2025-11-23 20:14:26 +0800

    三数之和

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -7,6 +7,39 @@
 using namespace std;
 
 
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<vector<int>> ret;
+
+//         int n = nums.size();
+//         for(int i = 0;i < n - 2;i++){
+//             int x = nums[i];
+
+//             if(i && x == nums[i - 1]) continue;
+//             if(x + nums[i + 1] + nums[i + 2] > 0) break;
+//             if(x + nums[n - 1] + nums[n - 2] < 0) continue;
+
+//             int j = i + 1,k = n - 1;
+
+//             while(j < k){
+//                 if(x + nums[j] + nums[k] < 0) j++;
+//                 else if(x + nums[j] + nums[k] > 0) k--;
+//                 else
+//                 {
+//                     ret.push_back({x,nums[j],nums[k]});
+
+//                     for(j++;j < k && nums[j] == nums[j - 1] ;j++);
+//                     for(k--;j < k && nums[k] == nums[k + 1] ;k--);
+//                 }
+//             }
+//         }
+
+//         return ret;
+//     }
+// };
+
 // /**
 //  * Definition for a binary tree node.
 //  * struct TreeNode {
