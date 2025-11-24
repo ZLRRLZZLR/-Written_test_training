f0d7d24aadc3db2d36e7928a1baee9764db3fb69
zhou-liurong <19551230598@163.com>
2025-11-24 18:01:43 +0800

    颜色分类

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         int left = -1,right = n;
+//         int cur = 0;
+//         while(cur < right){
+//             if(nums[cur] == 0){
+//                 swap(nums[++left],nums[cur++]);
+//             }
+//             else if(nums[cur] == 2){
+//                 swap(nums[--right],nums[cur]);
+//             }
+//             else{
+//                 cur++;
+//             }
+//         }
+//     }
+// };
 
 // class Solution {
 // public:
