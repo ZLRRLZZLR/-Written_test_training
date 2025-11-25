e002533349517c7c812cbf341b360e98de9722aa
zhou-liurong <19551230598@163.com>
2025-11-25 21:19:04 +0800

    搜索二维矩阵II

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,7 +6,20 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int i = matrix.size() - 1,j = 0;
 
+//         while(i >= 0 && j < matrix[0].size()){
+//             if(matrix[i][j] < target) j++;
+//             else if(matrix[i][j] > target) i--;
+//             else return true;
+//         }
+
+//         return false;
+//     }
+// };
 // /**
 //  * Definition for singly-linked list.
 //  * struct ListNode {
