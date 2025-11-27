b8136e276996e742d7e8def509c8b3988d5cfa79
zhou-liurong <19551230598@163.com>
2025-11-27 23:23:23 +0800

    反转链表

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,38 @@
 
 using namespace std;
 
+// /**
+//  * struct ListNode {
+//  *	int val;
+//  *	struct ListNode *next;
+//  *	ListNode(int x) : val(x), next(nullptr) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param head ListNode类 
+//      * @return ListNode类
+//      */
+//     ListNode* ReverseList(ListNode* head) {
+//         ListNode* newhead = new ListNode(-1);
+//         ListNode* cur = head;
+
+//         while(cur){
+//             ListNode* tmp = cur->next;
+//             cur->next = newhead->next;
+//             newhead->next = cur;
+//             cur = tmp;
+//         }
+
+//         return newhead->next;
+//     }
+// };
+
+
 // class Solution {
 // public:
 //     bool searchMatrix(vector<vector<int>>& matrix, int target) {
