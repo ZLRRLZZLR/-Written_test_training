c75a86aed53b3f0caa9a604ab3a116120fa93298
zhou-liurong <19551230598@163.com>
2025-11-29 21:10:38 +0800

    链表内指定区间反转

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -20,6 +20,57 @@ using namespace std;
 //      *
 //      * 
 //      * @param head ListNode类 
+//      * @param m int整型 
+//      * @param n int整型 
+//      * @return ListNode类
+//      */
+//     ListNode* reverseBetween(ListNode* head, int m, int n) {
+
+//         //不需要反转的情况
+//         if(nullptr == head || m == n){
+//             return head;
+//         }
+
+//         //哨兵节点方便处理特殊情况
+//         ListNode* newhead = new ListNode(-1);
+//         newhead->next = head;
+//         ListNode* prev = newhead;
+
+//         //
+//         for(int i = 1;i < m;i++){
+//             prev = prev->next;
+//         }
+
+//         //反转区间链表
+//         head = prev->next;
+//         for(int i = m;i < n;i++){
+//             ListNode* next = head->next;
+//             head->next = next->next;
+//             next->next = prev->next;
+//             prev->next = next;
+//         }
+
+//         head = newhead->next;
+//         delete newhead;
+//         return head;
+//     }
+// };
+
+
+// /**
+//  * struct ListNode {
+//  *	int val;
+//  *	struct ListNode *next;
+//  *	ListNode(int x) : val(x), next(nullptr) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param head ListNode类 
 //      * @return ListNode类
 //      */
 //     ListNode* ReverseList(ListNode* head) {
