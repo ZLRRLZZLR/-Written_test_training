32198b766212b5b278779f7aa368c707d11679ce
zhou-liurong <19551230598@163.com>
2025-12-04 23:39:34 +0800

    算术平方根

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,7 +6,21 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if (x < 1) return 0;
+//         long long left = 0, right = x;
+//         while (left < right)
+//         {
+//             long long mid = left + ((right - left + 1) >> 1);
+//             if (mid * mid <= x) left = mid;
+//             else right = mid - 1;
+//         }
 
+//         return left;
+//     }
+// };
 // /**
 //  * struct ListNode {
 //  *	int val;
