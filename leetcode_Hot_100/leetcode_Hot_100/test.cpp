95326dcac7a0cf550cdeed1c5adbd9b84ccd784a
zhou-liurong <19551230598@163.com>
2025-12-14 10:21:00 +0800

    判断链表中是否有环

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode * slow = head,*fast = head;
+//         while(fast&&fast->next){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(fast == slow) return true;
+//         }
+//         return false;
+//     }
+// };
+
 // class Solution {
 // public:
 //     int longestOnes(vector<int>& nums, int k) {
