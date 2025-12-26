605b7b1418e58698f46e7b410a8585e7a7344335
zhou-liurong <19551230598@163.com>
2025-12-26 23:34:35 +0800

    链表的奇偶重排

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,44 @@
 
 using namespace std;
 
+
+// /**
+//  * struct ListNode {
+//  *	int val;
+//  *	struct ListNode *next;
+//  *	ListNode(int x) : val(x), next(nullptr) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param head ListNode类 
+//      * @return ListNode类
+//      */
+//     ListNode* oddEvenList(ListNode* head) {
+//         // write code here
+//         if(head == nullptr){
+//             return head;
+//         }
+
+//         ListNode* even = head->next;
+//         ListNode* odd = head;
+//         ListNode* evenhead = even;
+
+//         while(even&&even->next){
+//             odd->next = even->next;
+//             odd = odd->next;
+//             even->next = odd->next;
+//             even = even->next;
+//         }
+//         odd->next = evenhead;
+//         return head;
+//     }
+// };
+
 // /**
 //  * struct ListNode {
 //  *	int val;
