8888d9e53e31bf9699c75a4b1fd76ff37b73e260
zhou-liurong <19551230598@163.com>
2025-12-27 23:51:12 +0800

    二分查找

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param nums int整型vector 
+//      * @param target int整型 
+//      * @return int整型
+//      */
+//     int search(vector<int>& nums, int target) {
+//         // write code here
+//         int left = 0,right = nums.size() - 1;
+//         while(left <= right){
+//             int mid = left + (right - left) / 2;
+//             if(nums[mid] == target) return mid;
+//             else if(nums[mid] > target) right = mid - 1;
+//             else left = mid + 1; 
+//         }
+//         return -1;
+//     }
+// };
 
 // /**
 //  * struct ListNode {
