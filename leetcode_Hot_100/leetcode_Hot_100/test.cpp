67cccb5daee4de1de55aa8b07a0c2ca4b2bf6783
zhou-liurong <19551230598@163.com>
2025-12-31 23:47:39 +0800

    二叉树的前序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,7 +6,36 @@
 
 using namespace std;
 
-
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * };
+//  */
+// class Solution {
+//     vector<int> ret;
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param root TreeNode类 
+//      * @return int整型vector
+//      */
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         // write code here
+//         preorder(root);
+//         return ret;
+//     }
+//     void preorder(TreeNode*root){
+//         if(root == nullptr) return;
+//         ret.push_back(root->val);
+//         preorder(root->left);
+//         preorder(root->right);
+//     }
+// };
 // class Solution {
 // public:
 //     /**
