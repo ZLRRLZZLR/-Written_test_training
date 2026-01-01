7dd03898864bb26c6ef3ead5c7a37422ff24ce42
zhou-liurong <19551230598@163.com>
2026-01-01 17:53:35 +0800

    后序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,38 @@
 
 using namespace std;
 
+
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * };
+//  */
+// class Solution {
+//     vector<int> ret;
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param root TreeNode类 
+//      * @return int整型vector
+//      */
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         // write code here
+//         inorder(root);
+//         return ret;
+//     }
+//     void inorder(TreeNode* root){
+//         if(root == nullptr) return ;
+//         inorder(root->left);
+//         ret.push_back(root->val);
+//         inorder(root->right);
+//     }
+// };
+
 // /**
 //  * struct TreeNode {
 //  *	int val;
