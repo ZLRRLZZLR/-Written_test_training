976e94be73646c44719e94f01ec80d487d6d6d31
zhou-liurong <19551230598@163.com>
2026-01-01 17:50:35 +0800

    中序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -24,6 +24,38 @@ using namespace std;
 //      * @param root TreeNode类 
 //      * @return int整型vector
 //      */
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         // write code here
+//         postorder(root);
+//         return ret;
+//     }
+//     void postorder(TreeNode* root){
+//         if(root == nullptr) return ;
+//         postorder(root->left);
+//         postorder(root->right);
+
+//         ret.push_back(root->val);
+//     }
+// };
+
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * };
+//  */
+// class Solution {
+//     vector<int> ret;
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param root TreeNode类 
+//      * @return int整型vector
+//      */
 //     vector<int> preorderTraversal(TreeNode* root) {
 //         // write code here
 //         preorder(root);
