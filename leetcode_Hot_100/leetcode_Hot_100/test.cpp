855a39a77d5ea45dd7db38d18ab0436e18e9198d
zhou-liurong <19551230598@163.com>
2026-01-06 20:55:25 +0800

    二叉树的镜像

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -11,6 +11,37 @@ using namespace std;
 //  *	int val;
 //  *	struct TreeNode *left;
 //  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * }; 
+//  */
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param pRoot TreeNode类 
+//      * @return TreeNode类
+//      */
+//     TreeNode* Mirror(TreeNode* pRoot) {
+//         // write code here
+//         if(pRoot == nullptr) return nullptr;
+
+//         TreeNode* pleft = pRoot->left;
+//         TreeNode* pright = pRoot->right;
+
+//         pRoot->right = Mirror(pleft);
+//         pRoot->left = Mirror(pright);
+
+//         return pRoot;
+//     }
+};
+
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
 //  * };
 //  */
 // /**
