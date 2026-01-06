04f4a3749bdcc0ad64e2ab71aa7a782c536a3e82
zhou-liurong <19551230598@163.com>
2026-01-06 20:56:22 +0800

    判断是不是二叉搜索树

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -12,6 +12,34 @@ using namespace std;
 //  *	struct TreeNode *left;
 //  *	struct TreeNode *right;
 //  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * };
+//  */
+// class Solution {
+//     long prev = INT_MIN;
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param root TreeNode类 
+//      * @return bool布尔型
+//      */
+//     bool isValidBST(TreeNode* root) {
+//         // write code here
+//         if(root==nullptr) return true;
+//         if(!isValidBST(root->left)) return false;
+//         if(prev >= root->val) return false;
+//         prev = root->val;
+//         return isValidBST(root->right);
+//     }
+// };
+
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 //  * }; 
 //  */
 // class Solution {
