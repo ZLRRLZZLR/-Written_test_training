f0b3a71834fb1439de5b7cdd92f8dae110170a5b
zhou-liurong <19551230598@163.com>
2026-01-09 23:01:42 +0800

    跳台阶

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -12,6 +12,30 @@ using namespace std;
 //      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 //      *
 //      * 
+//      * @param number int整型 
+//      * @return int整型
+//      */
+//     int jumpFloor(int number) {
+//         // write code here
+//         if(number == 1) return 1;
+//         if(number == 2) return 2;
+//         int a = 1,b = 2;
+//         int c = 0;
+//         for(int i = 3;i <= number;i++){
+//             c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return c;
+//     }
+// };
+
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
 //      * @param nums int整型vector 
 //      * @return int整型
 //      */
