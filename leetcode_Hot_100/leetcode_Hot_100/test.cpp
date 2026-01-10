2897cb2c4437168b5da1a25382b3adc1d3e491af
zhou-liurong <19551230598@163.com>
2026-01-10 11:13:17 +0800

    最长公共子序列

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,49 @@
 
 using namespace std;
 
+// #include <algorithm>
+// class Solution {
+//   public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * longest common subsequence
+//      * @param s1 string字符串 the string
+//      * @param s2 string字符串 the string
+//      * @return string字符串
+//      */
+//     string LCS(string s1, string s2) {
+//         // write code here
+//         int n = s1.size();
+//         int m = s2.size();
+//         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= m; j++) {
+//                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//                 if (s1[i - 1] == s2[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 }
+//             }
+//         }
+
+//         string ans;
+//         int i = n, j = m;
+//         while (dp[i][j]) {
+//             if (dp[i][j] == dp[i - 1][j]) {
+//                 i--;
+//             } else if (dp[i][j] == dp[i][j - 1]) {
+//                 j--;
+//             } else if (dp[i][j] == dp[i - 1][j - 1] + 1) {
+//                 ans += s1[i - 1];
+//                 i--;
+//                 j--;
+//             }
+//         }
+//         if (ans.size() == 0) return "-1";
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
 
 // class Solution {
 // public:
