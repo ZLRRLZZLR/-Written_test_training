33df7a21958b7c220fd16a63d758cae09ed9748b
zhou-liurong <19551230598@163.com>
2026-01-25 17:53:28 +0800

    在二叉树中找到两个节点的最近公共祖先

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,42 @@
 
 using namespace std;
 
+// /**
+//  * struct TreeNode {
+//  *	int val;
+//  *	struct TreeNode *left;
+//  *	struct TreeNode *right;
+//  *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     /**
+//      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//      *
+//      * 
+//      * @param root TreeNode类 
+//      * @param o1 int整型 
+//      * @param o2 int整型 
+//      * @return int整型
+//      */
+//     int lowestCommonAncestor(TreeNode* root, int o1, int o2) {
+//         // write code here
+//         return dfs(root,o1,o2)->val;
+//     }
+
+//     TreeNode* dfs(TreeNode* root, int o1, int o2){
+//         if(root == nullptr || root->val == o1 || root->val == o2) return root;
+
+//         TreeNode* left = dfs(root->left,o1,o2);
+//         TreeNode* right = dfs(root->right,o1,o2);
+
+//         if(left != nullptr && right != nullptr) return root;
+
+//         return left != nullptr ? left:right;
+//     }
+// };
+
 // class Solution {
 // public:
 //     /**
