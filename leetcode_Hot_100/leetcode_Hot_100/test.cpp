9b33335efc8347c4141884e73909e8e8d0f3a621
zhou-liurong <19551230598@163.com>
2026-02-09 00:55:03 +0800

    最短无序连接子序列

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -9,6 +9,28 @@ using namespace std;
 // class Solution {
 // public:
 //     int findUnsortedSubarray(vector<int>& nums) {
+//         int n = nums.size();
+//         int mn = nums[n - 1], l = n;
+//         for(int i = n - 1;i >= 0;--i){
+//             if(nums[i] > mn){
+//                 l = i;
+//             }
+//             mn = min(nums[i],mn);
+//         }
+//         int mx = nums[0],r = 0;
+//         for(int i = 0;i < n;i++){
+//             if(nums[i] < mx){
+//                 r = i;
+//             }
+//             mx = max(nums[i],mx);
+//         }
+//         return max(r - l + 1,0);
+//     }
+// };
+
+// class Solution {
+// public:
+//     int findUnsortedSubarray(vector<int>& nums) {
 //         vector<int> ret = nums;
 //         sort(ret.begin(), ret.end());
 //         int left = 0, right = nums.size() - 1;
