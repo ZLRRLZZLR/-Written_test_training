dfc54692bc2c6677de989dcc0a613e6050fe58d6
zhou-liurong <19551230598@163.com>
2026-02-14 08:50:41 +0800

    反转链表

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,31 @@
 
 using namespace std;
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* newhead = new ListNode(-1);
+//         ListNode* cur = head;
+//         while(cur){
+//             ListNode* tmp = cur->next;
+//             cur->next = newhead->next;
+//             newhead->next = cur;
+//             cur = tmp;
+//         }
+//         return newhead->next;
+//     }
+// };
+
 // class Solution {
 // public:
 //     int removeDuplicates(vector<int>& nums) {
