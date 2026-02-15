8cfc6137597a5d797a31eaa327225cbeb5873189
zhou-liurong <19551230598@163.com>
2026-02-15 15:32:39 +0800

    第二高的薪水

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+// # Write your MySQL query statement below
+// select max(distinct salary) as SecondHighestSalary from Employee where salary < (select max(distinct salary) from Employee)
+
+
 // /**
 //  * Definition for singly-linked list.
 //  * struct ListNode {
