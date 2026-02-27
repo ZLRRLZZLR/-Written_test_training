e9e2747facf581ed391d2931601a7de9bcffed99
zhou-liurong <19551230598@163.com>
2026-02-27 15:19:46 +0800

    从前序与中序遍历序列构造

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -18,6 +18,42 @@ using namespace std;
 //  * };
 //  */
 // class Solution {
+//     TreeNode* _buildTree(vector<int>& preorder, vector<int>& inorder,int& previ,int begini,int endi){
+//         if(begini > endi){
+//             return nullptr;
+//         }
+
+//         TreeNode* root = new TreeNode(preorder[previ]);
+//         int cur = begini;
+//         while(inorder[cur] != preorder[previ]){
+//             cur++;
+//         }
+//         previ++;
+
+//         root->left = _buildTree(preorder, inorder, previ,begini,cur - 1);
+//         root->right = _buildTree(preorder, inorder, previ, cur + 1,endi);
+//         return root;
+//     }
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         int i = 0;
+//         return _buildTree(preorder,inorder,i,0,inorder.size() - 1);
+//     }
+// };
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
 //     vector<int> ret;
 // public:
 //     vector<int> postorderTraversal(TreeNode* root) {
