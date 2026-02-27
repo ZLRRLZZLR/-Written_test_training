158a3712574c48b63102065a9ba116563eaa6bdc
zhou-liurong <19551230598@163.com>
2026-02-27 15:04:39 +0800

    二叉树的后续遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -7,6 +7,34 @@
 using namespace std;
 
 // /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//     vector<int> ret;
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         Traversal(root);
+//         return ret;
+//     }
+
+//     void Traversal(TreeNode* root){
+//         if(root == nullptr) return ;
+//         Traversal(root->left);
+//         Traversal(root->right);
+//         ret.push_back(root->val);
+//     }
+// };
+
+
+// /**
 //  * Definition for singly-linked list.
 //  * struct ListNode {
 //  *     int val;
