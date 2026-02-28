0be22cbc523a41bce162aad382c65b5e429155d3
zhou-liurong <19551230598@163.com>
2026-02-28 16:15:41 +0800

    从中序与后续遍历序列构造二叉树

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,41 @@
 
 using namespace std;
 
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//     TreeNode* _buildTree(vector<int>& inorder, vector<int>& postorder,int& posti,int begini,int endi){
+//         if(begini > endi) return nullptr;
+
+//         TreeNode* root = new TreeNode(postorder[posti]);
+
+//         int cur = begini;
+//         while(inorder[cur] != postorder[posti]){
+//             cur++;
+//         }
+
+//         posti--;
+
+//         root->right = _buildTree(inorder,postorder,posti,cur + 1,endi);
+//         root->left = _buildTree(inorder,postorder,posti,begini,cur - 1);
+
+//         return root;
+//     }
+// public:
+//     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+//         int i = postorder.size() - 1;
+//         return _buildTree(inorder, postorder,i,0,inorder.size() - 1);
+//     }
+// };
 
 // /**
 //  * Definition for a binary tree node.
