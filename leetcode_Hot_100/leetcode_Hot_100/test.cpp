9036e0cfcfe217b3b5c5d8acf7a59632766203ec
zhou-liurong <19551230598@163.com>
2026-03-02 12:01:26 +0800

    格雷编码

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,17 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     vector<int> grayCode(int n) {
+//         vector<int> ret;
+//         for(int i = 0;i < 1 << n;i++){
+//             ret.push_back(i ^ (i >> 1));
+//         }
+//         return ret;
+//     }
+// };
+
 // /**
 //  * Definition for a binary tree node.
 //  * struct TreeNode {
