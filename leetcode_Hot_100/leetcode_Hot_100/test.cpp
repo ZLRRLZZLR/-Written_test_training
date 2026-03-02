3bc56234656f212305459b294ce131652e13e919
zhou-liurong <19551230598@163.com>
2026-03-02 10:24:57 +0800

    路径总和II

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,44 @@
 
 using namespace std;
 
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+// class Solution {
+//     vector<vector<int>> ret;
+
+// public:
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+//         vector<int> path;
+//         int sum = targetSum;
+//         dfs(root, sum, path);
+//         return ret;
+//         ;
+//     }
+//     void dfs(TreeNode* root, int sum, vector<int>& path) {
+//         if (root == nullptr)
+//             return;
+//         path.push_back(root->val);
+//         sum -= root->val;
+//         if (root->left == nullptr && root->right == nullptr && sum == 0) {
+//             ret.push_back(path);
+//         } else {
+//             dfs(root->left, sum, path);
+//             dfs(root->right, sum, path);
+//         }
+//         path.pop_back();
+//     }
+// };
+
 // class Solution {
 //     bool isroot(string& root, string& word) {
 //         for (int i = 0; i < root.size(); i++) {
