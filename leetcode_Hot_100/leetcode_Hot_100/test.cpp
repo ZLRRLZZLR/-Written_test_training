e8e03038a3730ff8ec77ae7b9445af481c3e8182
zhou-liurong <19551230598@163.com>
2026-03-03 13:59:02 +0800

    二叉树的锯齿形层序遍历

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,47 @@
 
 using namespace std;
 
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//         if(root == nullptr) return {};
+//         queue<TreeNode*> q;
+//         vector<vector<int>> ret;
+//         q.push(root);
+//         int level = 0;
+//         int levelsize = 1;
+//         while (q.size()) {
+//             vector<int> tmp;
+//             while (levelsize--) {
+//                 auto front = q.front();
+//                 q.pop();
+//                 tmp.push_back(front->val);
+//                 if (front->left)
+//                     q.push(front->left);
+//                 if (front->right)
+//                     q.push(front->right);
+//             }
+//             levelsize = q.size();
+//             if(level % 2 != 0) reverse(tmp.begin(),tmp.end());
+//             level++;
+//             ret.push_back(tmp);
+//         }
+//         return ret;
+//     }
+// };
+
 // class Solution {
 // public:
 //     vector<int> grayCode(int n) {
