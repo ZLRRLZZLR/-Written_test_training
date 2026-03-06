5a60b9fa20b96b556f0905c1a72c679558d7bfd6
zhou-liurong <19551230598@163.com>
2026-03-06 15:33:33 +0800

    LRU缓存

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,49 @@
 
 using namespace std;
 
+// class LRUCache {
+// public:
+//     LRUCache(int capacity) : capacity(capacity) {}
+
+//     int get(int key) {
+//         auto itor = key_to_itor.find(key);
+//         if (itor == key_to_itor.end()) {
+//             return -1;
+//         }
+//         auto list_iter = itor->second;
+//         cache_list.splice(cache_list.begin(), cache_list, list_iter);
+//         return list_iter->second;
+//     }
+
+//     void put(int key, int value) {
+//         auto itor = key_to_itor.find(key);
+//         if (itor != key_to_itor.end()) {
+//             auto list_iter = itor->second;
+//             list_iter->second = value;
+//             cache_list.splice(cache_list.begin(), cache_list, list_iter);
+//             return;
+//         }
+//         cache_list.push_front({key, value});
+//         key_to_itor[key] = cache_list.begin();
+//         if (key_to_itor.size() > capacity) {
+//             key_to_itor.erase(cache_list.back().first);
+//             cache_list.pop_back();
+//         }
+//     }
+
+// private:
+//     int capacity;
+//     list<pair<int, int>> cache_list;
+//     unordered_map<int, list<pair<int, int>>::iterator> key_to_itor;
+// };
+
+// /**
+//  * Your LRUCache object will be instantiated and called as such:
+//  * LRUCache* obj = new LRUCache(capacity);
+//  * int param_1 = obj->get(key);
+//  * obj->put(key,value);
+//  */
+
 // class Solution {
 // public:
 //     string removeKdigits(string num, int k) {
