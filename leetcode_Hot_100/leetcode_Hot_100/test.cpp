ceeb69c7179a374e1e838cb217dd7cd63d9bd532
zhou-liurong <19551230598@163.com>
2026-03-09 15:47:17 +0800

    杨辉三角

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,23 @@
 
 using namespace std;
 
+
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> ret(numRows);
+//         for(int i = 0;i < numRows;i++){
+//             ret[i].resize(i + 1,1);
+//         }
+//         for(int i = 2;i < numRows;i++){
+//             for(int j = 1;j < i;j++){
+//                 ret[i][j] = ret[i - 1][j - 1] + ret[i - 1][j];
+//             }
+//         }
+//         return ret;
+//     }
+// };
+
 // /**
 //  * Definition for a binary tree node.
 //  * struct TreeNode {
