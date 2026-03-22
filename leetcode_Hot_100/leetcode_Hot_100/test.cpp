c9063a4e0ed4481598968915219b0a99685e85a6
zhou-liurong <19551230598@163.com>
2026-03-22 15:33:40 +0800

    最长递增子序列

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -7,6 +7,22 @@
 using namespace std;
 
 
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         vector<int> tails;
+//         for(auto& num : nums){
+//             auto it = lower_bound(tails.begin(),tails.end(),num);
+//             if(it != tails.end()){
+//                 *it = num;
+//             }else{
+//                 tails.push_back(num);
+//             }
+//         }
+//         return tails.size();
+//     }
+// };
+
 // #include <climits>
 // #include <iostream>
 // #include<vector>
