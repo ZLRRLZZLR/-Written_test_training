4d97519716a2d70d59cf3b1f4edb38e76ecdc1dd
zhou-liurong <19551230598@163.com>
2026-03-25 20:36:40 +0800

    最小栈

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -6,6 +6,48 @@
 
 using namespace std;
 
+// class MinStack {
+// public:
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(min.empty() || val <= min.top()){
+//             min.push(val);
+//         }
+//         st.push(val);
+//     }
+    
+//     void pop() {
+//         int top = st.top();
+//         if(top == min.top()){
+//             min.pop();
+//         }
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return min.top();
+//     }
+//     stack<int> st;
+//     stack<int> min;
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
 // class Solution {
 // public:
 //     int firstMissingPositive(vector<int>& nums) {
