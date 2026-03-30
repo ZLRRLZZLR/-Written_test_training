df9031a2db8184be6dfc7f2fdfcca3331162abfc
zhou-liurong <19551230598@163.com>
2026-03-30 21:27:43 +0800

    矩阵置零

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,57 @@ using namespace std;
 
 // class Solution {
 // public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         bool row_is_zero = false;
+//         bool col_is_zero = false;
+//         int m = matrix.size(), n = matrix[0].size();
+//         for (int i = 0; i < m; i++) {
+//             if (matrix[i][0] == 0) {
+//                 row_is_zero = true;
+//                 break;
+//             }
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (matrix[0][i] == 0) {
+//                 col_is_zero = true;
+//                 break;
+//             }
+//         }
+
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     matrix[i][0] = 0;
+//                     matrix[0][j] = 0;
+//                 }
+//             }
+//         }
+
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         if (row_is_zero) {
+//             for (int i = 0; i < m; i++) {
+//                 matrix[i][0] = 0;
+//             }
+//         }
+
+//         if (col_is_zero) {
+//             for (int i = 0; i < n; i++) {
+//                 matrix[0][i] = 0;
+//             }
+//         }
+//     }
+// };
+
+
+// class Solution {
+// public:
 //     bool is_palindrome(const string& s, int left, int right) {
 //         while(left < right){
 //             if(s[left++] != s[right--]){
