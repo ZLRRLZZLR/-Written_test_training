73b262a12df4256fd30df551ae1089093a493fe5
zhou-liurong <19551230598@163.com>
2026-04-07 19:36:06 +0800

    零钱兑换

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,23 @@ using namespace std;
 
 // class Solution {
 // public:
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n = coins.size();
+//         vector<int> dp(amount + 1,0x3f3f3f3f);
+//         dp[0] = 0;
+
+//         for(int i = 1;i <= n;i++){
+//             for(int j = 1;j <= amount;j++){
+//                 if(j >= coins[i - 1])
+//                 dp[j] = min(dp[j],dp[j - coins[i - 1]] + 1);
+//             }
+//         }
+//         return dp[amount] >= 0x3f3f3f3f ? -1 : dp[amount];
+//     }
+// };
+
+// class Solution {
+// public:
 //     int numSquares(int n) {
 //         int k = sqrt(n);
 //         vector<int> dp(n + 1,0x3f3f3f3f);
