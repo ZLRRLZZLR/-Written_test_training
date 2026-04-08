7010727375f8681eec650cf5eeb278a96c772168
zhou-liurong <19551230598@163.com>
2026-04-08 18:29:32 +0800

    乘积最大子数组

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 // class Solution {
 // public:
+//     int maxProduct(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> f(n + 1,1);
+//         vector<int> g(n + 1,1);
+//         int ret = INT_MIN;
+//         for(int i = 1;i <= n;i++){
+//             f[i] = max({nums[i - 1],f[i - 1] * nums[i - 1],g[i - 1] * nums[i - 1]});
+//             g[i] = min({nums[i - 1],f[i - 1] * nums[i - 1],g[i - 1] * nums[i - 1]});
+//             ret = max({f[i],g[i],ret});
+//         }
+//         return ret;
+//     }
+// };
+
+// class Solution {
+// public:
 //     int minDistance(string word1, string word2) {
 //         int n = word1.size();
 //         int m = word2.size();
