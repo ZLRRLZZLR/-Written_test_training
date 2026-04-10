7e08aa98f2bd019085e0c1a3cf70177e3de51419
zhou-liurong <19551230598@163.com>
2026-04-10 20:04:00 +0800

    寻找旋转排序数组中的最小值

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,24 @@ using namespace std;
 
 // class Solution {
 // public:
+//     int findMin(vector<int>& nums) {
+//         int left = 0,right = nums.size() - 1;
+//         int n = nums.size();
+//         while(left < right){
+//             int mid = (left + right) / 2;
+//             if(nums[mid] > nums[n - 1]){
+//                 left = mid + 1;
+//             }
+//             else{
+//                 right = mid;
+//             }
+//         }
+//         return nums[left];
+//     }
+// };
+
+// class Solution {
+// public:
 //     int search(vector<int>& nums, int target) {
 //         int left = 0, right = nums.size() - 1;
 
