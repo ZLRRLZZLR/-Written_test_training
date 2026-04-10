45836c1d8e97c3cb4b62258f6d478e9c502cdc8e
zhou-liurong <19551230598@163.com>
2026-04-10 19:21:36 +0800

    搜索旋转排序数组

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,35 @@ using namespace std;
 
 // class Solution {
 // public:
+//     int search(vector<int>& nums, int target) {
+//         int left = 0, right = nums.size() - 1;
+
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+//             if (nums[mid] == target)
+//                 return mid;
+//             if (nums[mid] < nums[right]) {
+//                 if (nums[mid] < target && target <= nums[right]) {
+//                     left = mid + 1;
+
+//                 } else {
+//                     right = mid - 1;
+//                 }
+//             } else {
+//                 if (nums[mid] > target && target >= nums[left]) {
+//                     right = mid - 1;
+//                 } else {
+//                     left = mid + 1;
+//                 }
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
+// class Solution {
+// public:
 //     int maxProduct(vector<int>& nums) {
 //         int n = nums.size();
 //         vector<int> f(n + 1,1);
