2ea402771bd2b1836ae1b9eea9738965fbebdd33
zhou-liurong <19551230598@163.com>
2026-04-11 18:43:32 +0800

    分割等和子集

--- leetcode_Hot_100/leetcode_Hot_100/test.cpp
+++ leetcode_Hot_100/leetcode_Hot_100/test.cpp
@@ -8,6 +8,25 @@ using namespace std;
 
 // class Solution {
 // public:
+//     bool canPartition(vector<int>& nums) {
+//         int sum = 0;
+//         for(auto num : nums)
+//             sum += num;
+//         if(sum % 2 != 0) return false;
+//         sum /= 2;
+//         vector<bool> dp(sum + 1);
+//         dp[0] = true;
+//         for(int i = 0;i < nums.size();i++){
+//             for(int j = sum;j >= nums[i];j--){
+//                 dp[j] = dp[j] | dp[j - nums[i]];
+//             }
+//         }
+//         return dp[sum];
+//     }
+// };
+
+// class Solution {
+// public:
 //     string decodeString(string s) {
 //         int i = 0;
 //         return dfs(s,i);
